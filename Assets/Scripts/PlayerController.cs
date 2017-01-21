52e4beaf87e573d3d1370846f774df0339dc48ba
harrison.pedro <hpedro1195@gmail.com>
2017-01-21 01:25:54 -0200

    asdfasdf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public Rigidbody2D rb;
+	public float moveSpeed = 0f;
+
+	private Rigidbody2D rb;
+	private SpriteRenderer sr;
+	private float moveDirection = 0f;
+	public float jumpSpeed = 0f;
+	private bool facingRight = true;
+	public bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		moveDirection = Input.GetAxis ("Horizontal");
+		this.Move (moveDirection);
+		this.Jump ();
 	}
 
-	private void Move(){
-	
+	private void Move(float moveDirection){
+		if (moveDirection < 0)
+			facingRight = false;
+		else
+			facingRight = true;
+
+		//this.ChangeFacingDirection ();
+		rb.velocity = new Vector2 (moveDirection * moveSpeed, rb.velocity.y);
+		print (rb.velocity);
+
 	}
+
+	private void Jump(){
+		if (!isGrounded ) {
+			return;
+		} else if(Input.GetKeyDown (KeyCode.Space)){
+			rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
+			isGrounded = false;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			isGrounded = false;
+		}
+	}
+
+	/*private void ChangeFacingDirection(){
+		sr.flipX = facingRight;
+	}*/
 }
