b3e459af4205897472ba3420b8b10834cba18350
Felipe <wowaccount123555@hotmail.com>
2017-01-21 00:32:25 -0200

    eu não sei

---
+++ Assets/Scripts/MagnetController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Side { Right, Left, Up, Down }
+public enum Polarity {Positive, Negative}
+
+public class MagnetController : MonoBehaviour {
+
+
+
+	public Side side;
+	public Polarity polarity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
